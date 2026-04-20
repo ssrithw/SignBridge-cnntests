68f39c9d4f0f203ee908802aceb690b6626b45cc
Shivangi Sritharan <ssrith@proton.me>
2026-04-20 10:51:21 +0530

    recreated db from scratch

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+'''
+config.py
+Created by Shivangi Sritharan
+Last modified: 10/04/2026
+
+This file contains configuration stuff.
+'''
+
 import os
 from dotenv import load_dotenv
 
@@ -7,7 +15,7 @@ load_dotenv(os.path.join(basedir, '.env')) # set variables before class is const
 class Config:
     # secret key for token generation
     # todo: add a real secret key lol
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "you-will-never-guess"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # the flask-SQLAlchemy extension takes the location of the application's 
     # database from the SQLALCHEMY_DATABASE_URI configuration variable
@@ -16,8 +24,7 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://signbridge:thesbdatabase123@localhost:5432/signbridgedb'
 
-    uri = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
+    uri = os.environ.get('DATABASE_URL')
     if not uri:
         raise RuntimeError("DATABASE_URL is not set. Application will not start.")
 
@@ -31,9 +38,9 @@ class Config:
     RATELIMIT_STORAGE_URI = os.environ.get("RATELIMIT_STORAGE_URI", "memory://")
     
     # admin email mailing list lol
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = "587"
-    MAIL_USE_TLS = "1"
-    MAIL_USERNAME = "admin.signbridge@gmail.com"
-    MAIL_PASSWORD = "gtoa yyxu nigi ptai"
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['admin.signbridge+errors@gmail.com']
